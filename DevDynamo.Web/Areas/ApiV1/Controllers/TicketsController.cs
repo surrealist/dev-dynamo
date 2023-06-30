1904b71f2651709b1efb10a3d9c41e6f2a1d2876
Samorn Kampisan <samorn@scbam.onmicrosoft.com>
2023-06-30 11:13:18 +0700

    create ChangeTicketStatus

---
+++ DevDynamo.Web/Areas/ApiV1/Controllers/TicketsController.cs
@@ -0,0 +1,54 @@
+﻿using DevDynamo.Models;
+using DevDynamo.Web.Areas.ApiV1.Models;
+using DevDynamo.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
+
+namespace DevDynamo.Web.Areas.ApiV1.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        private readonly AppDb db;
+        public TicketsController(AppDb db)
+        {
+            this.db = db;
+        }
+
+        //PUT /api/v1/tickets/{ticket_id}/status/{target_status_name}
+        [HttpPut("{ticket_id}/status/{target_status_name}")]
+        public ActionResult<TicketResponse> ChangeTicketStatus(int ticket_id,string target_status_name)
+        {
+            try
+            {
+                if (target_status_name == "") throw new InvalidOperationException("Status not found.");
+
+                var item = db.Tickets.SingleOrDefault(x => x.Id == ticket_id);
+                if (item is null)
+                {
+                   return NotFound(new ProblemDetails() { Title = $"Ticket with Id = {ticket_id} not found" });
+                }
+
+
+
+                var t = new Ticket();
+                t = item;
+                t.Status = target_status_name;
+
+                db.Tickets.Update(t);
+                db.SaveChanges();
+
+                var res = TicketResponse.FromModel(item);
+                return res;
+            }
+            catch (Exception)
+            {
+                return NotFound(new ProblemDetails() { Title = "Error updating data" });
+            }
+           
+
+
+        }
+       
+    }
+}
---
+++ DevDynamo.Web/Areas/ApiV1/Models/TicketResponse.cs
@@ -0,0 +1,25 @@
+﻿using DevDynamo.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevDynamo.Web.Areas.ApiV1.Models
+{
+    public class TicketResponse
+    {
+        public int Id { get; set; }
+        [StringLength(100)] public string Title { get; set; }
+        public string? Description { get; set; }
+        [StringLength(50)] public string Status { get; set; } = null!;
+        public static TicketResponse FromModel(Ticket T)
+        {
+            return new TicketResponse
+            {
+                Id = T.Id,
+                Title = T.Title,
+                Description = T.Description,
+                Status = T.Status
+            };
+
+        }
+
+    }
+}
