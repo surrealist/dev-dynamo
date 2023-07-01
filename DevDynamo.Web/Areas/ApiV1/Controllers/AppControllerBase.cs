8384d540f61814e3de629921e377e2c40e645250
Teerawut Phonghan <teerawutp@scbam.onmicrosoft.com>
2023-07-01 10:32:07 +0700

    ..

---
+++ DevDynamo.Web/Areas/ApiV1/Controllers/AppControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevDynamo.Web.Areas.ApiV1.Controllers
+{
+    public class AppControllerBase : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
